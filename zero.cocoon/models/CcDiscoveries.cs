9cdfc53a8fc1daef4ae10b19a82a0636f843a97f
Slavic Brutalik <sky.captin@gmail.com>
2021-11-21 15:28:58 +0200

    zero: fixed build error

--- zero.cocoon/models/CcDiscoveries.cs
+++ zero.cocoon/models/CcDiscoveries.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Google.Protobuf;
-using Org.BouncyCastle.Tls.Crypto.Impl.BC;
 using Proto;
 using SimpleBase;
 using zero.cocoon.autopeer;
