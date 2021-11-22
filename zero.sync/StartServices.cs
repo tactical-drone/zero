fc28a1c1d448e294be099d0f216acf8ef868c030
Slavic Brutalik <sky.captin@gmail.com>
2021-11-22 17:55:38 +0200

    ci: removed references to unused projects

--- zero.sync/StartServices.cs
+++ zero.sync/StartServices.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using zero.cocoon.events.services;
-using zero.tangle.api.controllers.bootstrap;
-using zero.tangle.api.controllers.services;
 
 namespace zero.sync
 {
