c577264f73de36c0a01ed374241de7a2fb324dac
Slavic Brutalik <sky.captin@gmail.com>
2022-03-31 19:10:31 +0200

    zero: minor udp improvements

--- zero.core/network/ip/IoUdpSocket.cs
+++ zero.core/network/ip/IoUdpSocket.cs
@@ -406,7 +406,7 @@ namespace zero.core.network.ip
                 args.SetBuffer(buf);
                 args.RemoteEndPoint = endPoint;
 
-                var sent = new ValueTask<bool>(taskCore, (short)taskCore.Version);
+                var sent = new ValueTask<bool>(taskCore, 0);
                 //receive
                 if (NativeSocket.SendToAsync(args) && !await sent.FastPath())
                     return 0;
@@ -453,7 +453,7 @@ namespace zero.core.network.ip
             try
             {
                 var tcs = (IoManualResetValueTaskSource<bool>)eventArgs.UserToken;
-                tcs.SetResult(!Zeroed() && eventArgs.SocketError == SocketError.Success && tcs.GetStatus((short)tcs.Version) == System.Threading.Tasks.Sources.ValueTaskSourceStatus.Pending);
+                tcs.SetResult(!Zeroed() && eventArgs.SocketError == SocketError.Success);
             }
             catch(Exception) when(Zeroed()){}
             catch(Exception e) when (!Zeroed())
@@ -501,7 +501,7 @@ namespace zero.core.network.ip
                             throw new OutOfMemoryException(nameof(_recvArgs));
                         var recvSource = new IoManualResetValueTaskSource<bool>();
                         
-                        var receiveAsync = new ValueTask<bool>(recvSource, (short)recvSource.Version);
+                        var receiveAsync = new ValueTask<bool>(recvSource, 0);
                         IValueTaskSource<bool> recvSourceRef = recvSource;
 
                         static void SetRef(ref IValueTaskSource<bool> s, SocketAsyncEventArgs a)
