0197d430c534e224642f8d1bd7c5cbac9643cfcc
Slavic Brutalik <brutal@slave.org>
2025-12-16 01:56:37 +0200

    core: fixed release build

--- zero.core.feat/models/protobuffer/CcProtocMessage.cs
+++ zero.core.feat/models/protobuffer/CcProtocMessage.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
